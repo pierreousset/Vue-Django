9e7868c64be030e46a557bd2fc3ba88fda19c642
Ousset <pierreousset@gmail.com>
2018-01-22 10:27:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Vue-Django
+
+### Features
+
+* Django backend in `./backend`
+* vuejs frontend in `./frontend`
+* Hot-reload with vue-loader
+* eslint linter integration
+* Makefile to make your life easy
+
+
+### Development environment setup
+
+```bash
+make dev
+make migrate
+make run
+```
+
+### Deployment
+
+```bash
+make prod
+make build
+```
+
---
+++ frontend/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <div>
+            <h1>{{ msg }}</h1>
+            </br>
+            <input v-model="newItem">
+            <button @click="addItemToList">Add</button>
+            <!-- displays list -->
+            <ul>
+                <li v-for="item in listItems">{{ item }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            msg: 'My To Do List',
+            listItems: ['buy food', 'play games', 'sleep'],
+            newItem: ''
+        }
+    },
+     methods: {
+      addItemToList() {
+          this.listItems.push(this.newItem);
+          this.newItem = '';
+      }
+  }
+}
+</script>
